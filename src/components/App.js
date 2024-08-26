e62775311e56c30249925ca7e856aaef4b91cd19
joycewangui <joycewangui590@gmail.com>
2024-08-26 09:28:34 +0300

    fetching API

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App(){
+    const [dogImage, setDogImage] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(true);
+
+
+    useEffect(() => {
+
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => {
+            setDogImage(data.message);
+            setIsLoaded(false);
+    }, [])})
+    
+
+
+    return (
+        <div>
+            {isLoaded ? (
+            <p>Loading...</p>):(
+            <img src={dogImage} alt="A Random Dog" />   )}
+        </div>
+    );
+    }
+
+
+export default App
